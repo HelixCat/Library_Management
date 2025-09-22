5ec3e8dd88a5058a582908122546c70fd15c13c7
Mahdi Ghomsheh <kitkat.helix@gmail.com>
2025-09-22 15:12:24 +0330

    feat: enhance service logging with AOP and add Swagger configuration

--- src/main/java/com/mahdi/website/concerns/ServiceLoggingAspect.java
+++ src/main/java/com/mahdi/website/concerns/ServiceLoggingAspect.java
@@ -2,14 +2,13 @@ package com.mahdi.website.concerns;
 
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 import java.util.Date;
 import java.util.Objects;
 import java.util.logging.Logger;
+
+import org.slf4j.LoggerFactory;
 
 @Aspect
 @Component
@@ -19,16 +18,21 @@ public class ServiceLoggingAspect {
     private final LogHandler logHandler;
     private final String packageName = "Service";
     private static final Logger logger = Logger.getLogger(ServiceLoggingAspect.class.getName());
+    private static final org.slf4j.Logger slf4jLogger = LoggerFactory.getLogger(ServiceLoggingAspect.class);
 
 
-    @Before("execution(* com.mahdi.website.service..*(..))")
-    public void loggBeforeCreateUser(JoinPoint joinPoint) {
+    @Pointcut("execution(* com.mahdi.website.service..*(..))")
+    public void serviceMethods() {}
+
+    @Before("serviceMethods()")
+    public void logBefore(JoinPoint joinPoint) {
         StringBuilder stringBuilder = logHandler.prepareLog(joinPoint, packageName, null, null);
         logger.info(stringBuilder.toString());
         logHandler.mangoDBLoggSaver(new Date(), "INFO", stringBuilder.toString());
+        slf4jLogger.info("A service method is about to execute.");
     }
 
-    @AfterReturning(pointcut = "execution(* com.mahdi.website.service..*(..))", returning = "result")
+    @AfterReturning(pointcut = "serviceMethods()", returning = "result")
     public void loggAfterCreateUser(JoinPoint joinPoint, Object result) {
         if (Objects.nonNull(result)) {
             StringBuilder stringBuilder = logHandler.prepareLog(joinPoint, packageName, result.toString(),  null);
@@ -37,10 +41,19 @@ public class ServiceLoggingAspect {
         }
     }
 
-    @AfterThrowing(pointcut = "execution(* com.mahdi.website.service..*(..))", throwing = "ex")
+    @After("serviceMethods()")
+    public void logAfter(JoinPoint joinPoint) {
+        StringBuilder stringBuilder = logHandler.prepareLog(joinPoint, packageName, null, null);
+        logger.info(stringBuilder.toString());
+        logHandler.mangoDBLoggSaver(new Date(), "INFO", stringBuilder.toString());
+        slf4jLogger.info("A service method has executed.");
+    }
+
+    @AfterThrowing(pointcut = "serviceMethods()", throwing = "ex")
     public void logAfterThrowingCreateUser(JoinPoint joinPoint, Exception ex){
         StringBuilder stringBuilder = logHandler.prepareLog(joinPoint, packageName, null, ex);
         logger.info(stringBuilder.toString());
         logHandler.mangoDBLoggSaver(new Date(), "ERROR", stringBuilder.toString());
+        slf4jLogger.error("An exception occurred in a service method: {}", ex.getMessage());
     }
 }
---
+++ src/main/java/com/mahdi/website/configuration/swagger/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.mahdi.website.configuration.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Library Management API")
+                        .version("1.0.0")
+                        .description("API documentation for the Library Management System"));
+    }
+}
