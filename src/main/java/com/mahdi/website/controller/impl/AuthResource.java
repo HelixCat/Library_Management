a4455901a10013a3a3cd960aaba77f1d7390c85a
Mahdi Ghomsheh <kitkat.helix@gmail.com>
2025-10-04 18:55:44 +0330

    refactor

--- src/main/java/com/mahdi/website/controller/impl/AuthResource.java
+++ src/main/java/com/mahdi/website/controller/impl/AuthResource.java
@@ -10,11 +10,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,7 +37,6 @@ public class AuthResource implements AuthRemote {
                 new UsernamePasswordAuthenticationToken(userDTO.getUsername(), userDTO.getPassword())
         );
 
-        // Generate JWT
         String token = jwtUtil.generateToken(authentication.getName());
 
         return ResponseEntity.ok(new AuthResponse(token, authentication.getName()));
