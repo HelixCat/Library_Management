60bd889377f4fa95ba9ae9a86f331f44cabc540a
Mahdi Ghomsheh <kitkat.helix@gmail.com>
2025-10-12 15:08:25 +0330

    solve some bugs and change the PublisherResource

--- src/main/java/com/mahdi/website/web/resource/PublisherResource.java
+++ src/main/java/com/mahdi/website/web/resource/PublisherResource.java
@@ -1,15 +1,17 @@
 package com.mahdi.website.web.resource;
 
-import com.mahdi.website.web.remote.PublisherRemote;
 import com.mahdi.website.dto.PublisherDTO;
-import com.mahdi.website.mapper.PublisherMapper;
 import com.mahdi.website.service.interfaces.PublisherService;
+import com.mahdi.website.web.remote.PublisherRemote;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -19,31 +21,30 @@ import java.util.List;
 @Tag(name = "Publisher", description = "Publisher management operations")
 public class PublisherResource implements PublisherRemote {
 
-    private final PublisherMapper publisherMapper;
     private final PublisherService publisherService;
 
     @Override
     @Operation(summary = "Search publishers", description = "Search for publishers based on provided criteria")
     public ResponseEntity<List<PublisherDTO>> searchPublishers(@RequestBody PublisherDTO publisherDTO) {
-        return new ResponseEntity<>(publisherMapper.toDTOList(publisherService.searchPublisher(publisherDTO)), HttpStatus.FOUND);
+        return new ResponseEntity<>(publisherService.searchPublisherDTO(publisherDTO), HttpStatus.FOUND);
     }
 
     @Override
     @Operation(summary = "Save new publisher", description = "Add a new publisher to the system")
     public ResponseEntity<PublisherDTO> savePublisher(@RequestBody PublisherDTO publisherDTO) {
-        return new ResponseEntity<>(publisherMapper.toDTO(publisherService.savePublisher(publisherDTO)), HttpStatus.CREATED);
+        return new ResponseEntity<>(publisherService.savePublisherDTO(publisherDTO), HttpStatus.CREATED);
     }
 
     @Override
     @Operation(summary = "Update publisher", description = "Update an existing publisher's information")
     public ResponseEntity<PublisherDTO> updatePublisher(@RequestBody PublisherDTO publisherDTO) {
-        return new ResponseEntity<>(publisherMapper.toDTO(publisherService.updatePublisher(publisherDTO)), HttpStatus.OK);
+        return new ResponseEntity<>(publisherService.updatePublisherDTO(publisherDTO), HttpStatus.OK);
     }
 
     @Override
     @Operation(summary = "Find publisher by ID", description = "Retrieve a publisher by their ID")
     public ResponseEntity<PublisherDTO> findPublisherById(@PathVariable Long id) {
-        return new ResponseEntity<>(publisherMapper.toDTO(publisherService.findPublisherById(id)), HttpStatus.OK);
+        return new ResponseEntity<>(publisherService.findPublisherDTOById(id), HttpStatus.OK);
     }
 
     @Override
